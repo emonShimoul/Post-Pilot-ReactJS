65d2002d003c5c2b315257f5389a4ce83af7f8ab
emonShimoul <emonshimoul@gmail.com>
2026-04-19 02:15:11 +0600

    added react router, tailwind + create folder structure and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
+import Posts from "./pages/Posts";
+import Login from "./pages/Login";
+import About from "./pages/About";
 
 function App() {
-
   return (
-    <>
-      POST PILOT
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="posts" element={<Posts />} />
+        <Route path="about" element={<About />} />
+      </Route>
+
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='text-3xl text-red-800'>
+            NAVBAR
+        </div>
+    );
+};
+
+export default Navbar;
