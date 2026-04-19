5211ce6fd4824fed9ca8d857acaf17cb282bb457
emonShimoul <emonshimoul@gmail.com>
2026-04-19 13:39:58 +0600

    implemented search functionality

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import usePosts from '../hooks/usePosts';
 import PostForm from '../components/PostForm';
 import PostList from '../components/PostList';
 
 const Posts = () => {
     const {posts, setPosts, loading} = usePosts();
+    const [search, setSearch] = useState("");
 
     const handleAddPost = (newPost) => {
         setPosts(prev => [newPost, ...prev]);
@@ -14,12 +15,23 @@ const Posts = () => {
         setPosts(prev => prev.filter(post => post.id !== id));
     }
 
+    const filteredPosts = posts.filter(post =>
+        post.title.toLowerCase().includes(search.toLowerCase())
+    );
+
     if (loading) return <p>Loading...</p>;
 
     return (
         <div>
+            <input
+                type="text"
+                placeholder='Search posts...'
+                className='border p-2 w-full mb-4 rounded'
+                values={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
             <PostForm onAddPost={handleAddPost} />
-            <PostList posts={posts} onDelete={handleDelete} />
+            <PostList posts={filteredPosts} onDelete={handleDelete} />
         </div>
     );
 };
